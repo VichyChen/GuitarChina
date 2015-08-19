663de7e33a5fedcbc6f1d416c5a6c730becb3d41
Vichy_Chen <15813350363>
2015-08-19 23:52:20 +0800

    no message

---
+++ GuitarChina/GuitarChina/Network/GCNetworkAPI.h
@@ -0,0 +1,13 @@
+//
+//  GCNetworkAPI.h
+//  GuitarChina
+//
+//  Created by 陈大捷 on 15/8/19.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCNetworkAPI : NSObject
+
+@end
---
+++ GuitarChina/GuitarChina/Network/GCNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  GCNetworkManager.m
+//  GuitarChina
+//
+//  Created by 陈大捷 on 15/8/19.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import "GCNetworkManager.h"
+
+@implementation GCNetworkManager
+
+@end
