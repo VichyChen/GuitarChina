83c3654ce14c0c8cb06166b79c830c70094cfbfb
Vichy_Chen <15813350363>
2015-08-19 21:55:48 +0800

    no message

---
+++ GuitarChina/GuitarChina/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuitarChina
+//
+//  Created by 陈大捷 on 15/8/19.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GuitarChina/GuitarChina/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GuitarChina
+//
+//  Created by 陈大捷 on 15/8/19.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
