90fc0fb9ce3270ed95d806f22b410bdf25b3916f
Vichy_Chen <15813350363>
2015-08-20 17:13:37 +0800

    no message

---
+++ GuitarChina/GuitarChina/BaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableViewController.h
+//  GuitarChina
+//
+//  Created by mac on 15/8/20.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTableViewController : UITableViewController
+
+@end
