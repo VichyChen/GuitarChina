6310b98dcf4a96331b93887a3cf8ec0aca0ee435
Vichy_Chen <15813350363>
2015-08-20 18:05:45 +0800

    no message

---
+++ GuitarChina/GuitarChina/Model/HotThread.m
@@ -0,0 +1,13 @@
+//
+//  HotThread.m
+//  GuitarChina
+//
+//  Created by mac on 15/8/20.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import "HotThread.h"
+
+@implementation HotThread
+
+@end
--- GuitarChina/GuitarChina/Network/GCNetworkAPI.h
+++ GuitarChina/GuitarChina/Network/GCNetworkAPI.h
@@ -8,3 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+#define GCNETWORKAPI_SERVICE_URL @"http://bbs.guitarchina.com/api/mobile/index.php?"
+
+//查看热帖
+#define GCNETWORKAPI_GET_HOTTHREAD [NSString stringWithFormat:@"%@mobile=no&version=1&module=hotthread", GCNETWORKAPI_SERVICE_URL]
+
+//论坛模块列表
+#define GCNETWORKAPI_GET_FORUMINDEX [NSString stringWithFormat:@"%@mobile=no&version=1&module=forumindex", GCNETWORKAPI_SERVICE_URL]
+
+//论坛模块帖子列表
+#define GCNETWORKAPI_GET_FORUMDISPLAY(fid, pageIndex, pageSize) [NSString stringWithFormat:@"%@mobile=no&version=1&module=forumdisplay&submodule=checkpost&fid=%@&page=%@&tpp=%@&orderby=dateline", GCNETWORKAPI_SERVICE_URL, (fid), (pageIndex), (pageSize)]
+
+//查看帖子详情
+#define GCNETWORKAPI_GET_VIEWTHREAD(tid, pageIndex, pageSize) [NSString stringWithFormat:@"%@mobile=no&version=1&module=viewthread&submodule=checkpost&tid=%@&page=%@&ppp=%@", GCNETWORKAPI_SERVICE_URL, (tid), (pageIndex), (pageSize)]
+
+
+
