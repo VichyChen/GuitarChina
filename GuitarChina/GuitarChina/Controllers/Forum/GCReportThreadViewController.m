387d8d26f13940258fcb20cbdd704e02657ff1f4
Vichy_Chen <15813350363>
2015-11-02 15:17:51 +0800

    no message

---
+++ GuitarChina/GuitarChina/Controllers/Forum/GCReportThreadViewController.m
@@ -0,0 +1,37 @@
+//
+//  GCReportThreadViewController.m
+//  GuitarChina
+//
+//  Created by mac on 15/11/2.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import "GCReportThreadViewController.h"
+
+@interface GCReportThreadViewController ()
+
+@end
+
+@implementation GCReportThreadViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ GuitarChina/GuitarChina/Views/Forum/GCReportThreadView.m
@@ -0,0 +1,21 @@
+//
+//  GCReportThreadView.m
+//  GuitarChina
+//
+//  Created by mac on 15/11/2.
+//  Copyright (c) 2015年 陈大捷. All rights reserved.
+//
+
+#import "GCReportThreadView.h"
+
+@implementation GCReportThreadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
