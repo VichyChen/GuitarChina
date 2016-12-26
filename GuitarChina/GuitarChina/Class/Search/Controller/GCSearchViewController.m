60194259a5170ad561c96fdb41fbe402e85004ed
Your Name <825230378@qq.com>
2016-12-26 16:19:12 +0800

    no message

--- GuitarChina/GuitarChina/Class/Search/Controller/GCSearchViewController.m
+++ GuitarChina/GuitarChina/Class/Search/Controller/GCSearchViewController.m
@@ -19,15 +19,18 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
     GCSearchViewTypeBlank,
 };
 
-
-@interface GCSearchViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+@interface GCSearchViewController ()
 
 @property (nonatomic, strong) UITextField *searchTextField;
+
 @property (nonatomic, strong) UITableView *searchTableView;
+@property (nonatomic, strong) GCTableViewKit *searchTableViewKit;
 @property (nonatomic, strong) UITableView *historyTableView;
+@property (nonatomic, strong) GCTableViewKit *historyTableViewKit;
 
 @property (nonatomic, copy) void (^searchBlock)();
 @property (nonatomic, copy) void (^historyBlock)();
+
 @property (nonatomic, assign) NSInteger pageIndex;
 @property (nonatomic, strong) NSMutableArray *searchArray;
 @property (nonatomic, strong) NSMutableArray *historyArray;
@@ -40,10 +43,6 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.searchArray = [NSMutableArray array];
-    self.historyArray = [NSMutableArray array];
-    self.searchRowHeightArray = [NSMutableArray array];
-    
     [self configureNavigationBar];
     [self configureView];
     [self configureBlock];
@@ -53,123 +52,6 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.05 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         [self.searchTextField becomeFirstResponder];
     });
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-
-#pragma mark - UITableViewDataSource
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (tableView.tag == 0) {
-        return 1;
-    }
-    else {
-        return self.searchArray.count;
-    }
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (tableView.tag == 0) {
-        static NSString *identifier = @"GCHistoryCell";
-        GCHistoryCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-        if (!cell) {
-            cell = [[GCHistoryCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        }
-        [cell configure:self.historyArray];
-        @weakify(self);
-        cell.didSelectButtonBlock = ^(NSInteger index){
-            @strongify(self);
-            self.searchTextField.text = self.historyArray[index];
-            [self showView:GCSearchViewTypeSearch];
-            [self search:self.historyArray[indexPath.row]];
-        };
-
-        return cell;
-    }
-    else {
-        static NSString *identifier = @"GCSearchCell";
-        GCSearchCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-        if (!cell) {
-            cell = [[GCSearchCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        }
-        GCSearchModel *model = self.searchArray[indexPath.row];
-        cell.model = model;
-        
-        return cell;
-    }
-}
-
-#pragma mark - UITableViewDelegate
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (tableView.tag == 0) {
-        return [GCHistoryCell getCellHeightWithArray:self.historyArray].height;
-    }
-    else {
-        NSNumber *height = [self.searchRowHeightArray objectAtIndex:indexPath.row];
-        return [height floatValue];
-    }
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (tableView.tag == 0) {
-        if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
-            [cell setSeparatorInset:UIEdgeInsetsMake(0, 10, 0, 10)];
-        }
-        if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
-            [cell setLayoutMargins:UIEdgeInsetsMake(0, 10, 0, 10)];
-        }
-    }
-    else {
-        if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
-            [cell setSeparatorInset:UIEdgeInsetsMake(0, 0, 0, 0)];
-        }
-        if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
-            [cell setLayoutMargins:UIEdgeInsetsMake(0, 0, 0, 0)];
-        }
-    }
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [self.searchTextField endEditing:YES];
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (tableView.tag == 0) {
-        /*
-        self.searchTextField.text = self.historyArray[indexPath.row];
-        [self showView:GCSearchViewTypeSearch];
-        [self search:self.historyArray[indexPath.row]];
-         */
-    }
-    else {
-        GCThreadDetailViewController *controller = [[GCThreadDetailViewController alloc] init];
-        GCSearchModel *model = self.searchArray[indexPath.row];
-        controller.tid = model.tid;
-        [self.navigationController pushViewController:controller animated:YES];
-    }
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    if (self.searchTextField.isEditing) {
-        [self.searchTextField endEditing:YES];
-    }
-}
-
-#pragma mark - UITextFieldDelegate
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField endEditing:YES];
-    if (textField.text.length > 0) {
-        [self showView:GCSearchViewTypeSearch];
-        [self search:textField.text];
-    }
-    
-    return YES;
 }
 
 #pragma mark - Private Methods
@@ -184,14 +66,12 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
 
     self.navigationItem.rightBarButtonItems = @[flexSpaceButton, closeButton];
     
+    UIView *leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 28, 28)];
     UIImageView *leftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(6, 6, 16, 16)];
     leftImageView.image = [[UIImage imageNamed:@"icon_search"] imageWithTintColor:[GCColor grayColor2]];
-    
-    UIView *leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 28, 28)];
     [leftView addSubview:leftImageView];
     
     self.searchTextField = [UIView createTextField:CGRectMake(0, 0, ScreenWidth, 28) borderStyle:UITextBorderStyleNone text:@"" textColor:[GCColor fontColor] placeholder:NSLocalizedString(@"Enter Keywords", nil) textAlignment:NSTextAlignmentLeft];
-    self.searchTextField.delegate = self;
     self.searchTextField.font = [UIFont systemFontOfSize:15];
     self.searchTextField.textColor = [GCColor fontColor];
     self.searchTextField.tintColor = [GCColor redColor];
@@ -202,6 +82,16 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
     self.searchTextField.leftView = leftView;
     self.searchTextField.returnKeyType = UIReturnKeySearch;
     [self.searchTextField addTarget:self action:@selector(searchTextFieldValueChange:) forControlEvents:UIControlEventEditingChanged];
+    @weakify(self);
+    self.searchTextField.bk_shouldReturnBlock = ^(UITextField *textField) {
+        @strongify(self);
+        [textField endEditing:YES];
+        if (textField.text.length > 0) {
+            [self showView:GCSearchViewTypeSearch];
+            [self search:textField.text];
+        }
+        return YES;
+    };
     self.navigationItem.titleView = self.searchTextField;
 }
 
@@ -229,7 +119,7 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
             self.searchTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
             self.searchTableView.footer = nil;
             
-            [self.searchArray removeAllObjects];
+            self.searchArray = [NSMutableArray array];
             [self.searchTableView reloadData];
             [self.searchTableView.header beginRefreshing];
         }
@@ -247,10 +137,9 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
                 return;
             }
             if (self.pageIndex == 1) {
-                [self.searchArray removeAllObjects];
                 self.searchArray = searchArray.datas;
                 
-                [self.searchRowHeightArray removeAllObjects];
+                self.searchRowHeightArray = [NSMutableArray array];
                 for (GCSearchModel *model in searchArray.datas) {
                     [self.searchRowHeightArray addObject:[NSNumber numberWithFloat:[GCSearchCell getCellHeightWithModel:model]]];
                 }
@@ -290,13 +179,6 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
     self.historyBlock = ^{
         @strongify(self);
         self.historyArray = [NSMutableArray arrayWithArray:[NSUD arrayForKey:kGCSearchHistory] ? [NSUD arrayForKey:kGCSearchHistory] : @[]];
-        //        if (self.searchTextField.text.length > 0) {
-        //            for (int i = (int)self.historyArray.count - 1; i >= 0; i--) {
-        //                if (![self.historyArray[i] containString:self.searchTextField.text]) {
-        //                    [self.historyArray removeObjectAtIndex:i];
-        //                }
-        //            }
-        //        }
         if (self.historyArray.count == 0) {
             [self showView:GCSearchViewTypeBlank];
         }
@@ -330,22 +212,16 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
         case GCSearchViewTypeHistory:
             self.historyTableView.hidden = NO;
             self.searchTableView.hidden = YES;
-            
             break;
             
         case GCSearchViewTypeSearch:
             self.historyTableView.hidden = YES;
             self.searchTableView.hidden = NO;
-            
             break;
             
         case GCSearchViewTypeBlank:
             self.historyTableView.hidden = YES;
             self.searchTableView.hidden = YES;
-            
-            break;
-            
-        default:
             break;
     }
 }
@@ -376,10 +252,8 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
 - (UITableView *)historyTableView {
     if (!_historyTableView) {
         _historyTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight - 64)];
-        _historyTableView.tag = 0;
-        _historyTableView.delegate = self;
-        _historyTableView.dataSource = self;
         _historyTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _historyTableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
         if ([_historyTableView respondsToSelector:@selector(setSeparatorInset:)]) {
             [_historyTableView setSeparatorInset:UIEdgeInsetsMake(0, 10, 0, 10)];
         }
@@ -388,15 +262,11 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
         }
         
         UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 44)];
-        
         UILabel *label = [UIView createLabel:CGRectMake(13, 0, ScreenWidth - 30, 44) text:NSLocalizedString(@"Search History", nil) font:[UIFont systemFontOfSize:15] textColor:[GCColor grayColor1]];
         [headerView addSubview:label];
-        
         _historyTableView.tableHeaderView = headerView;
         
-        
         UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 80)];
-        
         UIButton *button = [UIView createButton:CGRectMake(0, 0, ScreenWidth - 60, 40) buttonType:UIButtonTypeSystem text:NSLocalizedString(@"Clean History", nil) target:self action:@selector(clearHistory)];
         button.tintColor = [GCColor redColor];
         button.layer.cornerRadius = 5;
@@ -404,8 +274,32 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
         button.layer.borderWidth = 1;
         button.center = footerView.center;
         [footerView addSubview:button];
-        
         _historyTableView.tableFooterView = footerView;
+        
+        self.historyTableViewKit = [[GCTableViewKit alloc] initWithSystem];
+        @weakify(self);
+        self.historyTableViewKit.numberOfRowsInSectionBlock = ^(NSInteger section) {
+            return (NSInteger)1;
+        };
+        self.historyTableViewKit.cellForRowAtIndexPathBlock = ^(NSIndexPath *indexPath) {
+            @strongify(self);
+            GCHistoryCell *cell = [[GCHistoryCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"GCHistoryCell"];
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            [cell configure:self.historyArray];
+            cell.didSelectButtonBlock = ^(NSInteger index){
+                @strongify(self);
+                self.searchTextField.text = self.historyArray[index];
+                [self showView:GCSearchViewTypeSearch];
+                [self search:self.historyArray[indexPath.row]];
+            };
+            
+            return cell;
+        };
+        self.historyTableViewKit.heightForRowAtIndexPathBlock = ^(NSIndexPath *indexPath) {
+            @strongify(self);
+            return [GCHistoryCell getCellHeightWithArray:self.historyArray].height;
+        };
+        [self.historyTableViewKit configureTableView:_historyTableView];
     }
     return _historyTableView;
 }
@@ -413,10 +307,7 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
 - (UITableView *)searchTableView {
     if (!_searchTableView) {
         _searchTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight - 64)];
-        _searchTableView.tag = 1;
-        _searchTableView.delegate = self;
-        _searchTableView.dataSource = self;
-        
+        _searchTableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
         if ([_searchTableView respondsToSelector:@selector(setSeparatorInset:)]) {
             [_searchTableView setSeparatorInset:UIEdgeInsetsMake(0, 0, 0, 0)];
         }
@@ -426,6 +317,32 @@ typedef NS_ENUM(NSInteger, GCSearchViewType) {
         
         UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 0.5)];
         _searchTableView.tableFooterView = footerView;
+        
+        self.searchTableViewKit = [[GCTableViewKit alloc] initWithCellType:ConfigureCellTypeClass cellIdentifier:@"GCSearchCell"];
+        @weakify(self);
+        self.searchTableViewKit.getItemsBlock = ^{
+            @strongify(self);
+            return self.searchArray;
+        };
+        self.searchTableViewKit.cellForRowBlock = ^(NSIndexPath *indexPath, id item, UITableViewCell *cell) {
+            GCSearchCell *searchCell = (GCSearchCell *)cell;
+            GCSearchModel *model = item;
+            searchCell.model = model;
+        };
+        self.searchTableViewKit.heightForRowBlock = ^(NSIndexPath *indexPath, id item) {
+            @strongify(self);
+            NSNumber *height = [self.searchRowHeightArray objectAtIndex:indexPath.row];
+            return (CGFloat)[height floatValue];
+        };
+        self.searchTableViewKit.didSelectCellBlock = ^(NSIndexPath *indexPath, id item) {
+            @strongify(self);
+            [self.searchTextField endEditing:YES];
+            GCThreadDetailViewController *controller = [[GCThreadDetailViewController alloc] init];
+            GCSearchModel *model = item;
+            controller.tid = model.tid;
+            [self.navigationController pushViewController:controller animated:YES];
+        };
+        [self.searchTableViewKit configureTableView:_searchTableView];
     }
     return _searchTableView;
 }
