35d98e9cf84d7def4b2b26319bd41d3bffc023b1
Your Name <825230378@qq.com>
2017-01-30 22:54:45 +0800

    no message

---
+++ GuitarChina/GuitarChina/Class/News/Controller/GCNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCNewsViewController.h
+//  GuitarChina
+//
+//  Created by mac on 17/1/30.
+//  Copyright © 2017年 陈大捷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCNewsViewController : UIViewController
+
+@end
---
+++ GuitarChina/GuitarChina/Class/News/Controller/GCNewsViewController.m
@@ -0,0 +1,22 @@
+//
+//  GCNewsViewController.m
+//  GuitarChina
+//
+//  Created by mac on 17/1/30.
+//  Copyright © 2017年 陈大捷. All rights reserved.
+//
+
+#import "GCNewsViewController.h"
+
+@interface GCNewsViewController ()
+
+@end
+
+@implementation GCNewsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
